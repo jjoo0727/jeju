1fd6badb7b6f20f52327b38c35063d21ba3f8293
jjoo0727 <63052158+jjoo0727@users.noreply.github.com>
2024-02-06 01:05:44 +0900

    Add files via upload

--- Pangu/typhoon.py
+++ Pangu/typhoon.py
@@ -7,17 +7,24 @@ Created on Thu Jan 25 01:03:11 2024
 #%%
 import os
 import numpy as np
+
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from matplotlib.colors import LinearSegmentedColormap
+import matplotlib.colors as mcolors
 from mpl_toolkits.axes_grid1 import make_axes_locatable 
+import plotly.figure_factory as ff
+
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
-import plotly.figure_factory as ff
+
 import scipy.ndimage as ndimage
 from scipy.ndimage import binary_dilation
-import matplotlib.colors as mcolors
+
 from datetime import datetime, timedelta
+
+import tropycal.tracks as tracks
+
 import itertools
 
 
@@ -25,6 +32,19 @@ pangu_dir = r'/Data/home/jjoo0727/Pangu-Weather'
 
 lat_indices = np.linspace(90, -90, 721)
 lon_indices = np.linspace(-180, 180, 1441)[:-1]
+
+proj = ccrs.PlateCarree(central_longitude=180)
+original_cmap = plt.get_cmap("BrBG")
+truncated_BrBG = truncate_colormap(original_cmap, minval=0.35, maxval=1.0) #수증기 colormap 지정
+
+pres_list = ['1000','925','850','700','600','500','400','300','250','200','150','100','50']
+pres=500                                            #살펴볼 기압면 결정
+p=pres_list.index(str(pres))
+
+surface_factor = ['MSLP', 'U10', 'V10', 'T2M']
+surface_dict = {'MSLP':0, 'U10':1, 'V10':2, 'T2M':3}
+upper_factor = ['z', 'q', 't', 'u', 'v']
+upper_dict = {'z':0, 'q':1, 't':2, 'u':3, 'v':4}
 
 #위경도 범위 지정 함수
 def latlon_extent(lon_min, lon_max, lat_min, lat_max):    
@@ -45,7 +65,6 @@ def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
         'truncated_' + cmap.name, cmap(np.linspace(minval, maxval, n)))
     return new_cmap
 
-#와도 계산인데 좀 결과가 이상함??
 def calculate_vorticity(u, v, lat_grid, lon_grid):
     # Constants
     earth_radius = 6371e3  # in meters
@@ -73,18 +92,34 @@ def calculate_vorticity(u, v, lat_grid, lon_grid):
 
     return vorticity
 
+def storm_info(storm_name, storm_year):
+    ibtracs = tracks.TrackDataset(basin='all',source='ibtracs',ibtracs_mode='jtwc_neumann',catarina=True)
+    storm = ibtracs.get_storm((storm_name,storm_year))
+    storm.to_dataframe()
+    first_index = np.where((storm['wmo_vmax']>=35) == True)[0][0]
+    storm_lon = storm['lon'][first_index:]-180
+    storm_lat = storm['lat'][first_index:]
+    storm_mslp = storm['mslp'][first_index:]
+    storm_time = storm['time'][first_index:]
+    return storm_lon, storm_lat, storm_mslp, storm_time
+
 #태풍 발생 위치 주변 & 10m/s 이상 지역 주변
 def plot_min_value(ax, data, lat_indices, lon_indices, lat_start, lon_start, lat_grid, lon_grid, 
-                   wind_speed, pred_str, init_str, init_pos, min_position, mask_size=10, init_size=3):
+                   wind_speed, pred_str, storm_lon, storm_lat, storm_mslp, storm_time, min_position, mask_size=10, init_size=3):
     
     pred_str = datetime.strptime(pred_str, "%Y/%m/%d/%HUTC")
-    init_str = datetime.strptime(init_str, "%Y/%m/%d/%HUTC")
+    init_str = storm_time[0]
+    
     
     #태풍 발생 시점 이전엔 찾기 X
     if pred_str < init_str:
         return min_position
     
-
+    storm_lon = storm_lon[storm_time<=pred_str]
+    storm_lat = storm_lat[storm_time<=pred_str]
+    storm_mslp = storm_mslp[storm_time<=pred_str]
+    storm_time = storm_time[storm_time<=pred_str]
+ 
     # 데이터의 복사본 생성 및 마스킹
     data_copy = np.copy(data)
     wind_mask = wind_speed > 10         # wind_speed > 10인 조건을 만족하는 픽셀에 대한 마스크 생성
@@ -92,12 +127,12 @@ def plot_min_value(ax, data, lat_indices, lon_indices, lat_start, lon_start, lat
     data_copy[~expanded_wind_mask] = np.nan # 확장된 마스크를 사용하여 wind_speed > 10 조건과 그 주변 2픽셀 이외의 위치를 NaN으로 설정
     
     if len(min_position) < 1:
-        data_copy[(lat_grid > (init_pos[0]+init_size))|(lat_grid < (init_pos[0]-init_size))] = np.nan   
-        data_copy[(lon_grid > (init_pos[1]+init_size-180))|(lon_grid < (init_pos[1]-init_size-180))] = np.nan 
+        data_copy[(lat_grid > (storm_lat[0]+init_size))|(lat_grid < (storm_lat[0]-init_size))] = np.nan   
+        data_copy[(lon_grid > (storm_lon[0]+init_size))|(lon_grid < (storm_lon[0]-init_size))] = np.nan 
     
     
     if pred_str > init_str and min_position:
-        last_min_lon, last_min_lat, last_min_idx, _, _ = min_position[-1]
+        _, _, last_min_idx, _, _ = min_position[-1]
         row_start = max(0, last_min_idx[0] - mask_size)
         row_end = min(data_copy.shape[0], last_min_idx[0] + mask_size + 1)  # +1은 Python의 슬라이싱이 상한을 포함하지 않기 때문
         col_start = max(0, last_min_idx[1] - mask_size)
@@ -121,11 +156,6 @@ def plot_min_value(ax, data, lat_indices, lon_indices, lat_start, lon_start, lat
     norm_p = mcolors.Normalize(vmin=950, vmax=1020)
     
     for i, (lon, lat, idx, p_str, min_pres) in enumerate(min_position):
-        # 선으로 연결
-        if len(min_position) > 1:
-            lons, lats, _, p_strs, _ = zip(*min_position)
-            ax.plot(lons, lats, color='red', transform=proj, linestyle='-', marker='')
-
         if p_str.endswith('00UTC'):
             dx, dy = 3, -3  # 시간 나타낼 위치 조정
             new_lon, new_lat = lon + dx, lat + dy
@@ -136,14 +166,19 @@ def plot_min_value(ax, data, lat_indices, lon_indices, lat_start, lon_start, lat
                     color='red', horizontalalignment='center', verticalalignment='center', fontsize=8,
                     transform=ax.projection)
 
-    
+
     lons = [pos[0] for pos in min_position]
     lats = [pos[1] for pos in min_position]
     min_values = [pos[4]/100 for pos in min_position]
     
-    min_p = ax.scatter(lons, lats, c=min_values, cmap='jet_r', norm=norm_p, transform=proj, zorder=2)
+    if len(min_position) > 1:
+            ax.plot(lons, lats, color='red', transform=ax.projection, linestyle='-', marker='', label = 'model pred')
+    
+    min_p = ax.scatter(lons, lats, c=min_values, cmap='jet_r', norm=norm_p, transform=ax.projection, zorder=2)
     cbar_min_p = plt.colorbar(min_p, orientation='horizontal', fraction=0.046, pad=0.07)
     cbar_min_p.set_label('Minimum Pressure(hPa)', fontsize=16)
+    ax.plot(storm_lon, storm_lat, color='black', linestyle='-', marker='o', label = 'best track', transform=ax.projection, zorder=3)
+    ax.legend()
     return min_position
 
 #ax 배경 지정
@@ -191,47 +226,29 @@ def process_meteorological_data(surface, surface_dict, upper, upper_dict, lat_st
     vorticity = calculate_vorticity(u_850, v_850, lat_grid, lon_grid)
     
     return mslp, wind_speed, z_diff, vorticity
-#%%
-proj = ccrs.PlateCarree(central_longitude=180)
-original_cmap = plt.get_cmap("BrBG")
-truncated_BrBG = truncate_colormap(original_cmap, minval=0.35, maxval=1.0) #수증기 colormap 지정
 
-proj = ccrs.PlateCarree(central_longitude=180)  # Set central_longitude to 180
+
+#%%
 #위경도 범위 지정
 #동반구는 0~180, 서반구는 180~360y
 lat_start, lat_end, lon_start, lon_end, extent, latlon_ratio = latlon_extent(110,160,5,45)  
+lon_grid, lat_grid = np.meshgrid(lon_indices[lon_start:lon_end + 1], lat_indices[lat_start:lat_end + 1])
 
-pres_list = ['1000','925','850','700','600','500','400','300','250','200','150','100','50']
-pres=500                                            #살펴볼 기압면 결정
-p=pres_list.index(str(pres))
-
-surface_factor = ['MSLP', 'U10', 'V10', 'T2M']
-surface_dict = {'MSLP':0, 'U10':1, 'V10':2, 'T2M':3}
-upper_factor = ['z', 'q', 't', 'u', 'v']
-upper_dict = {'z':0, 'q':1, 't':2, 'u':3, 'v':4}
-
-predict_interval_list = np.arange(0,24*7+1,6)[1:]   #볼 예측 시간 지정
-# time_str = '2020/08/26/00UTC'                       #time_str 지정
+storm_lon, storm_lat, storm_mslp, storm_time = storm_info('maysak', 2020)   #태풍 영문명, 년도 입력
+predict_interval_list = np.arange(0,24*7+1,6)[1:]     #살펴볼 예측 시간 지정
 year = ['2020']
 month = ['08']
-day = ['28']
+day = ['26','27','28']
 times = ['00','06','12','18']
-init_str = '2020/08/28/06UTC'                       #태풍 시점
-init_pos = [16.93, 130.16]                          #태풍 첫 위경도
-# init_pos = [14.06, 135.71]                          #태풍 첫 위경도
 
 
 for y, m, d, tm in itertools.product(year, month, day, times):
+    #ERA5 initial map
     time_str = f'{y}/{m}/{d}/{tm}UTC'
     print(time_str)
     input_data_dir = rf'{pangu_dir}/input_data/{time_str}'
-
-
-    # 위경도 범위 설정
-    lat_start, lat_end, lon_start, lon_end, extent, latlon_ratio = latlon_extent(110, 160, 5, 45)
-    lon_grid, lat_grid = np.meshgrid(lon_indices[lon_start:lon_end + 1], lat_indices[lat_start:lat_end + 1])
+    
     min_position = []  # 최저값 위치를 저장할 리스트
-
 
     predict_str = time_str
     surface = np.load(os.path.join(input_data_dir, 'surface.npy')).astype(np.float32)  
@@ -242,7 +259,7 @@ for y, m, d, tm in itertools.product(year, month, day, times):
     fig, ax = plt.subplots(1, 1, figsize=(10*latlon_ratio, 10), subplot_kw={'projection': proj})
     ax.set_title(f'{time_str} Surface (+0h)', fontsize=20)
     min_position = plot_min_value(ax, mslp, lat_indices, lon_indices, lat_start, lon_start, lat_grid, lon_grid,
-                                                    wind_speed, predict_str, init_str, init_pos, min_position)
+                                                    wind_speed, predict_str, storm_lon, storm_lat, storm_mslp, storm_time, min_position)
     setup_map(ax)
     weather_map_contour(ax, lon_grid, lat_grid, mslp)
 
@@ -260,7 +277,7 @@ for y, m, d, tm in itertools.product(year, month, day, times):
     plt.close(fig)
     fig.savefig(f'{pangu_dir}/plot/typhoon/{time_str}/0h_vc.png')
 
-
+    #모델 예측 그리기
     for predict_interval in predict_interval_list:
 
         time_obj = datetime.strptime(time_str, "%Y/%m/%d/%HUTC")
@@ -276,7 +293,7 @@ for y, m, d, tm in itertools.product(year, month, day, times):
         fig, ax = plt.subplots(1, 1, figsize=(10*latlon_ratio, 10), subplot_kw={'projection': proj})
         ax.set_title(f'{time_str} (+{predict_interval}h)  predict: {predict_str}', fontsize=20)
         min_position = plot_min_value(ax, mslp, lat_indices, lon_indices, lat_start, lon_start, lat_grid, lon_grid,
-                                                        wind_speed, predict_str, init_str, init_pos, min_position)
+                                                        wind_speed, predict_str, storm_lon, storm_lat, storm_mslp, storm_time, min_position)
         setup_map(ax)
         weather_map_contour(ax, lon_grid, lat_grid, mslp)
 
@@ -284,15 +301,8 @@ for y, m, d, tm in itertools.product(year, month, day, times):
         # contourf = ax.contourf(lon_grid, lat_grid, wind_speed, cmap='jet', levels = np.linspace(0,30,7), alpha=0.5)
         # contourf = ax.contourf(lon_grid, lat_grid, z_diff, cmap='jet', levels = np.linspace(10000,11500,16), alpha=0.5)
         cbar = plt.colorbar(contourf, ax=ax, orientation='horizontal', extend='both', fraction=0.046, pad=0.04)
-        cbar.set_label('Vorticity', fontsize=16)  # You can customize the label here
+        cbar.set_label('Vorticity($S^{-1}$)', fontsize=16)  # You can customize the label here
         # cbar.set_label('200-850hPa Thickness(m)', fontsize=16)  # You can customize the label here
         # cbar.set_label('10m Wind Speed(m/s)', fontsize=16)  # You can customize the label here
         plt.close(fig)
         fig.savefig(f'{pangu_dir}/plot/typhoon/{time_str}/{predict_interval}h_vc.png')
-
-
-    
-    
-    
-    
-
